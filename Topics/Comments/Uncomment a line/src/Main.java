3b77ce75150b4fc1e94d35d3792b756992fee924
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-09-23 16:23:23 +0300

    comments

---
+++ Topics/Comments/Uncomment a line/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * The class implements a simple application with the main method.
+ */
+class CommentedSimpleApplication {
+
+    public static void main(String[] args) {
+        // System.out.println(980233);
+        final int n = 893_457;
+        System.out.println(n);
+        // System.out.println(789722);
+    }
+}
