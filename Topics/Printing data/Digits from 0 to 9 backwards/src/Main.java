32272ab018af88236e404273c24e8c198e8d89b6
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2022-03-04 10:42:37 +0200

    for loop magic number constant

---
+++ Topics/Printing data/Digits from 0 to 9 backwards/src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        final int end = 9;
+        for (int myNum = end; myNum >= 0; myNum--) {
+            System.out.print(myNum + " ");
+        }
+    }
+}
