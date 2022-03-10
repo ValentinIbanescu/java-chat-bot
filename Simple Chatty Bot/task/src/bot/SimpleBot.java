de6e78ff0bd42ca5267abe4551920ad7e8fb7858
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2022-03-10 15:43:19 +0200

    :rocket: my first Java chat bot is working

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -6,15 +6,17 @@ public class SimpleBot {
     final static Scanner scanner = new Scanner(System.in); // Do not change this line
 
     public static void main(String[] args) {
-        greet("Aid", "2018"); // change it as you need
+        greet("Aid", "2022"); // change it as you need
         remindName();
         guessAge();
         count();
+        test();
         // ...
         end();
     }
 
     static void greet(String assistantName, String birthYear) {
+        System.out.println();
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
@@ -45,10 +47,22 @@ public class SimpleBot {
 
     static void test() {
         System.out.println("Let's test your programming knowledge.");
-        // write your code here
+        System.out.println("What is the recommended Code style for a project involving external developers? (type a number)\n");
+        String[] answers = {"All programmers write code in the same style following one of generally accepted coding style guides",
+        "Every programmer has their own unique but consistent coding style", "Create your own style and follow it consistenly",
+        "No style is needed, every time you write code as you can to save the time and effort"};
+
+        for (int i = 0; i < answers.length; i++) {
+            System.out.println(i + 1 + ". " + answers[i]);
+        }
+        int input = scanner.nextInt();
+        while (input != 1) {
+            System.out.println("Wrong answer. Please try again.");
+            input = scanner.nextInt();
+        }
     }
 
     static void end() {
-        System.out.println("Congratulations, have a nice day!"); // Do not change this text
+        System.out.println("Congratulations, have a nice day!");
     }
 }
